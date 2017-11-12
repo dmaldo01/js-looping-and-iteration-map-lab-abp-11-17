0cb375d9d56a50ba9a565405c5509c905611203e
Daniel Maldonado <dmaldo01@calpoly.edu>
2017-11-12 18:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function lowerCaseDrivers(drivers){
   })
 }
 
-function nameToAttributes(drivers){
-  return names.map(function(name){
+function nameToAttributes(drivers)
+{
+  return drivers.map(function(name)
+  {
     let fname = name.split(" ")[0]
     let lname = name.split(" ")[1]
     return {firstName: fname, lastName: lname}
