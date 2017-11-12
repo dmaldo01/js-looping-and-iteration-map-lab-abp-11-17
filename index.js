7ee6450f342f2a84735f9e495ddd569ba510ef1d
Daniel Maldonado <dmaldo01@calpoly.edu>
2017-11-12 18:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Code your solution in this file.
-/*function lowerCaseDrivers(drivers){
+function lowerCaseDrivers(drivers){
   return drivers.map(function(name){
     return name.toLowerCase()
   })
 }
-*/
+
 function nameToAttributes(drivers){
   return names.map(function(name){
-    let fname = name.split(" ")(0)
-    let fname = name.split(" ")(2)
+    let fname = name.split(" ")[0]
+    let fname = name.split(" ")[1]
     return {firstName: fname, lastName: lname}
   })
 
